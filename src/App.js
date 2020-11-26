15ffcc664fa5a85c00a7e034fa04702601105127
davidor23 <david.o.reilly@outlook.com>
2020-11-26 14:26:07 +0000

    Repo commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Review from './Review';
+
+function App() {
+  return( 
+  
+  <main>
+    <div className="container">
+      <div className="title">
+        <h2>Our Reviews</h2>
+        <div className="underline"></div>
+        <Review/>
+      </div>
+    </div>
+    
+    
+  </main>
+      
+  )}
+
+export default App;
